1c2b42f2a2124507aac5f8e0de8760369ce36783
Milkah Mwangi <gathonimilkahh@gmail.com>
2025-12-05 10:27:27 +0300

    wrote my first ever test

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,19 @@
-// Your tests here
+//Your tests here
+
+import { pointsForWord } from "../utils";
+
+//describe is the grandparent function that takes the string of the function being tested and an anonymous function of a specific test as a parameter
+describe ("pointsForWord", () => {
+//it is the parent function that describes one specific test and takes the string description of said test as parameter and the actual code for the test as the second parameter
+it("calculates the total points for a word (1 point for vowels, 2 for consonants)", ()=> {
+
+//here i am defining the test value as the word test
+const word = "test";
+
+//here i am passing my newly created variable as an argument to the pointsForWord function and storing it as a variable called points
+const points = pointsForWord(word);
+
+expect(points).toBe(7); });
+});
+
+
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,16 @@
-// Your code here
+// // Your code here
+//telling the test file to export this function and passing the previously defined word as a parameter. but how will it be accessible and it is function scoped? ama it's cause we've called the function, here and it remembers. God, help?
+export function pointsForWord (word){
+//initializing the value of points to start at 0
+let points = 0
+//using a for of loop to dictate the point giving conditions
+for (const char of word){
+    if (["a", "e", "i", "o", "u"].includes (char)){
+        points +=1
+    }
+    else { points+=2;
+
+    }
+}
+return points;
+}
